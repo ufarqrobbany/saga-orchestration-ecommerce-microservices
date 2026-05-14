c9af7226fdebdd0b88ad244ac64b7596d7e7d28b
Umar Faruq Robbany <faruqrobbany@gmail.com>
2026-05-14 20:43:52 +0700

    fix: resolve docker build failures and missing dist folders

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from '../../../shared-library/src/filters/http-exception.filter';
+import { HttpExceptionFilter } from '../../shared-library/src/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
